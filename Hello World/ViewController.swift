8963b2ea173ec39b0865765d8fbd48fe03bec816
Dmitry Grinko <dm.grinko@gmail.com>
2022-02-16 12:48:35 -0800

    init

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -1,19 +1,43 @@
-//
-//  ViewController.swift
-//  Hello World
-//
-//  Created by Dmitry Grinko on 2/15/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = "https://raw.githubusercontent.com/dm-grinko/api/main/data.json"
+        getData(from: url)
     }
 
+    private func getData (from url: String) {
+        let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
+            guard let data = data, error == nil else {
+                print ("Something went wrong")
+                return
+            }
+            
+            var result: Response?
+            do {
+                result = try JSONDecoder().decode(Response.self, from: data)
+            }
+            catch {
+                print ("failed to convert \(error.localizedDescription)")
+            }
+            
+            guard let json = result else {
+                return
+            }
+            
+            print(json.name)
+            print(json.age)
+        })
+        
+        task.resume()
+    }
 
+}
+
+struct Response: Codable {
+    let name: String
+    let age: Int
 }
 
